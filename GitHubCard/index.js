0b2494f78b5c83c44838ba5856cf06c924e942e7
joeys1122 <josephs1122@gmail.com>
2021-12-02 14:55:58 -0500

    created five more cards from follower array

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -38,6 +38,16 @@ axios.get('https://api.github.com/users/joeys1122')
 
 const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
+followersArray.forEach(user => {
+  axios.get(`https://api.github.com/users/${user}`)
+    .then(resp => {
+      document.querySelector('.cards').appendChild(cardMaker(resp));
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
